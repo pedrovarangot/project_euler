e76b49e14739a30c8c8899e17478fe5dd998fde1
Pedro Varangot <peter@satellogic.com>
2017-08-23 01:01:11 -0300

    Modulo vs. substract in Project Euler 1

---
+++ 1/mults_s_15.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int cases;
+    
+    cin >> cases;
+    for(int i = 0; i < cases; ++i) {
+        int max;
+        cin >> max;
+        long sum = 0;
+        for(int j = 0; j < max; j += 3) {
+            sum += j;
+        }
+        for(int j = 0; j < max; j += 5) {
+            if(j % 3 == 0) continue;
+            sum += j;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}

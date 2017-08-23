7e4d928f46fe8d17952f453a702122ecad18b0d3
Pedro Varangot <peter@satellogic.com>
2017-08-23 02:15:29 -0300

    Naive sieve

---
+++ 3/sieve.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import sys
+import math
+
+def remove_multiples(factors, number):
+    for n in factors:
+        if n == number or n % number != 0:
+            yield n
+
+t = int(input().strip())
+for a0 in range(t):
+    num = int(input().strip())
+    factors = range(2, int(num/2) + 1)
+    sieve = list(factors)
+    sieve.append(num)
+    for n in sieve:
+        sieve = remove_multiples(sieve, n)
+    for n in sorted(sieve, reverse=True):
+        if num % n == 0:
+            print(n)
+            break

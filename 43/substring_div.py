58f629df4035f49ec77b23f3f4aba45cb11afc92
Pedro Varangot <pedrov@gmail.com>
2017-09-14 23:01:09 -0300

    Pandigital divisors with 0 allowed

--- 43/substring_div.py
+++ 43/substring_div.py
@@ -1,13 +1,16 @@
 from itertools import permutations
 
-A050278_list = [''.join(d) for d in permutations('0123456789', 10) if d[0] != '0'] # Chai Wah Wu, May 25 2015
-
 primes = {2:2,3:3,4:5,5:7,6:11,7:13,8:17}
 
 def test(n=9):
-	for pandigital in A050278_list:
+	numstr = ''
+	for i in range(n+1):
+		numstr += str(i)
+	print(numstr)
+	nums_list = [''.join(d) for d in permutations(numstr, n+1)]
+	for pandigital in nums_list:
 		doesit = True
-		for i in range(2,8+1):
+		for i in range(2,n):
 			doesit = (int(pandigital[i-1:i+2]) % primes[i]) == 0
 			if not doesit:
 				break

736f351b1c18e0bcbe80ff1aef6b746102f09e2f
Pedro Varangot <pedrov@gmail.com>
2017-09-30 12:58:32 -0300

    Pandigital multiples working in Euler++

--- 38/pandigitalmultiples.py
+++ 38/pandigitalmultiples.py
@@ -7,12 +7,42 @@ def domul(n, t):
 	return int(res)
 
 pandigitals19 = set([int(''.join(d)) for d in permutations('123456789', 9)])
+pandigitals18 = set([int(''.join(d)) for d in permutations('12345678', 8)])
 
-def test():
+def test9(n):
+	res = []
 	for i in range(2,6+1):
 		concat = range(1,i+1)
 		for j in range(1, 9 // i + 1):
 			all_candidates = [int(''.join(d)) for d in permutations('123456789', j)]
 			for candidate in all_candidates:
-				if domul(int(candidate), concat) in pandigitals19:
-					print(int(candidate), concat, domul(int(candidate), concat))
+				candidate = int(candidate)
+				mult = domul(candidate, concat)
+				if candidate < n and mult in pandigitals19:
+					res.append(candidate)
+					#print(int(candidate), concat, domul(int(candidate), concat))
+	return res
+
+def test8(n):
+	res = []
+	for i in range(2,6+1):
+		concat = range(1,i+1)
+		for j in range(1, 9 // i + 1):
+			all_candidates = [int(''.join(d)) for d in permutations('12345678', j)]
+			for candidate in all_candidates:
+				#print(candidate)
+				candidate = int(candidate)
+				mult = domul(candidate, concat)
+				if candidate < n and mult in pandigitals18:
+					res.append(candidate)
+					#print(int(candidate), concat, domul(int(candidate), concat))
+	return res
+
+def test():
+	n, k = map(int, input().strip().split())
+	if k == 9:
+		r = sorted(test9(n))
+	else:
+		r = sorted(test8(n))
+	for i in r:
+		print(i)
